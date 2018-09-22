21ef26be4961fca9f8f247c49240a330f5dbee98
Dorothy Leung <dorothyleung@LHM.local>
2018-09-21 18:28:09 -0700

    hw1 ruby symbol vs string

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,27 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    int_arr = a.map(&:to_i)
+    int_arr_two = int_arr.map{|i| i+2}.uniq
+
+    # even = []
+    # i = 0
+    # while i < int_arr_two.length
+    #   if int_arr_two[i] % 2 == 0
+    #     even.push(int_arr_two[i])
+    #   end
+    #   i += 1
+    # end
+
+    # Ruby way
+    even = []
+    int_arr_two.each do |item|
+      if item % 2 == 0
+        even.push(item)
+      end
+    end
+
+    even.reject{|number| number >= 10}.inject(:+)
+
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,19 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a + b) ** 2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  b = a.sort()
+  b.map { |i| i + 1 }
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  first_name + ' ' + last_name
 
 end
 
@@ -49,4 +53,6 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  sym = word.split('').map{|c| c.to_sym}.map{|s| values[s]}.inject(:+)
+
 end
